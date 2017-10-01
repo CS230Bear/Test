b9fcf59b3dd0a15033b32fca8f1f147f8fc930df
Gpoelsma <32401594+Gpoelsma@users.noreply.github.com>
2017-09-30 20:22:53 -0600

    Testing a java file upload

---
+++ Launcher.java
@@ -0,0 +1,16 @@
+package guil.prac2d.soft;
+
+import guil.prac2d.soft.display.Display;
+
+public class Launcher
+{
+	public static void main (String[] args)
+	
+		{
+		new Game("Tile Game!", 900, 600);
+		
+		
+		}
+			
+
+}
